12954162e21cac4a47fac77574104e65c28fa9d5
Andres Mrad (Q-ro) <andres.mrad@gmail.com>
2015-10-08 16:57:28 -0500

    The info update
    Added scripts that allow player to "shoot" planets to obtain info about
    them, added scripts to allow planets to display info to canvas elements

---
+++ Solar System Simulation/Assets/Scripts/DisplayInfo.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayInfo : MonoBehaviour
+{
+
+	// target impact on game
+    public string name;
+	public float radiusInfo = 0.0f;
+	public double distanceInfo = 0;
+
+
+
+	// when collided with another gameObject
+	void OnCollisionEnter (Collision newCollision)
+	{
+
+
+		// only do stuff if hit by a projectile
+        if (newCollision.gameObject.tag == "Selector")
+        {
+
+
+			// if game manager exists, make adjustments based on target properties
+			if (GameManager.gm) {
+                GameManager.gm.targetHit(name,radiusInfo, distanceInfo);
+			}
+
+            Destroy(newCollision.gameObject);
+		}
+	}
+
+}
---
+++ Solar System Simulation/Assets/Scripts/GameManager.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class GameManager : MonoBehaviour {
+
+	// make game manager public static so can access this from other scripts
+	public static GameManager gm;
+
+	// public variables	
+    public Text nameDisplay;
+	public Text radiusDisplay;
+	public Text distanceDisplay;
+
+	// setup the game
+	void Start () {
+		// get a reference to the GameManager component for use by other scripts
+		if (gm == null) 
+			gm = this.gameObject.GetComponent<GameManager>();	}
+
+	// this is the main game event loop
+	void Update () 
+    {
+
+	}
+
+
+	// public function that can be called to update the score or time
+    public void targetHit(string name,float radius, double distance)
+	{
+		// update the text UI
+        nameDisplay.text = name;
+        radiusDisplay.text = radius.ToString() + " km";
+        distanceDisplay.text = distance.ToString() + " km";
+	}
+
+	// public function that can be called to restart the game
+	public void RestartGame ()
+	{
+		// we are just loading a scene (or reloading this scene)
+		// which is an easy way to restart the level
+		Application.LoadLevel (0);
+	}	
+
+}
